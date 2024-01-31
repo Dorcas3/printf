960e3750d4258e45eaeb43fa7fc5ba86947ef292
ignatiusomeje <ignatiusomeje153@gmail.com>
2024-01-30 19:50:38 -0500

    fix error

--- _print-string.c
+++ _print-string.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
-* _print_string - handles printing only strings to the standard output
-* @str: list of arguments;
-* Return: int;
-*/
+ * _print_string - handles printing only strings to the standard output
+ * @str: list of arguments;
+ * Return: int;
+ */
 
 int _print_string(va_list str)
 {
 	char *string = va_arg(str, char *);
 	int str_len = 0;
 
-	while (string[str_len] != '\0')
-		str_len++;
-	write(1, string, str_len);
+	while (*string != '\0')
+	{
+		str_len += write(1, string, 1);
+		string++;
+	}
 	return (str_len);
 }
